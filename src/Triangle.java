5821b8bdcd73b410400a342b0afdb161833f7479
Aruna Gamage <admgbit@gmail.com>
2018-01-16 19:55:38 +0530

    first commit

---
+++ src/Triangle.java
@@ -0,0 +1,16 @@
+public class Triangle {
+
+    private String type;
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void draw() {
+        System.out.println(getType() + " Triangle drawn");
+    }
+}
